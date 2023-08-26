96017ea4669f6bcb9f7562cd8e90546904397242
fuzetto-pixel <mayconfuzetto@outlook.com>
2023-08-26 01:01:20 -0300

    comiit front

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+
+import TelaCadastrofuncao from "./telas/TelaCadastroFuncao.jsx";
+import TelaCadPessoa from "./telas/TelaCadPessoa.jsx";
+
+import TelaMenu from "./telas/TelaMenu";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+        <Routes>
+         
+          <Route path="/funcao" element={<TelaCadastrofuncao/>}/>     
+          <Route path="/pessoas" element={<TelaCadPessoa/>}/>
+          <Route path="/" element={<TelaMenu/>}/>
+          <Route path="*" element={<Tela404/>} />
+            
+        </Routes>
+        
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/templates/Menu.js
@@ -0,0 +1,39 @@
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { FaHome, FaSignOutAlt } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+
+
+export default function Menu(props) {
+  return (
+    <Navbar bg="success" variant="dark" expand="lg">
+      <Container>
+        <div>
+          <Link to="/" > <FaHome title="Página Inicial" style={{ cursor: 'pointer' }} size={30} color="white" /></Link>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Cadastros" id="basic-nav-dropdown">
+              <div>
+                <LinkContainer to="/pessoas">
+                  <NavDropdown.Item>Pessoas</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Divider />
+                <LinkContainer to="/funcao">
+                  <NavDropdown.Item>Cargos</NavDropdown.Item>
+                </LinkContainer>
+              </div>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        <Nav >
+          <div>
+            <FaSignOutAlt title="Sair" style={{ cursor: 'pointer' }} size={20} color="white"/>
+          </div>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
